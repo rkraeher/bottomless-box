be9466c9a4064fc7b510423adc06e1b0569cbe65
rudi kraeher <rudikraeher@gmail.com>
2024-01-04 17:19:06 +0100

    Input a search term and list the text results that are displayed in the popover

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,13 @@
 import { PlaywrightCrawler, log } from 'crawlee';
 import { router } from './routes';
 
-// This is better set with CRAWLEE_LOG_LEVEL env var
-// or a configuration option. This is just for show 😈
-log.setLevel(log.LEVELS.DEBUG);
+// log.setLevel(log.LEVELS.DEBUG);
 
-log.debug('Setting up crawler.');
+// log.debug('Setting up crawler.');
 const crawler = new PlaywrightCrawler({
-  // Instead of the long requestHandler with
-  // if clauses we provide a router instance.
   requestHandler: router,
   maxRequestsPerCrawl: 20,
+  headless: false,
 });
 
-await crawler.run(['https://warehouse-theme-metal.myshopify.com/collections']);
+await crawler.run(['https://store.epicgames.com/en-US/']);
--- src/routes.ts
+++ src/routes.ts
@@ -1,82 +1,101 @@
 import { createPlaywrightRouter, Dataset } from 'crawlee';
+import { Page } from 'playwright';
 
-// createPlaywrightRouter() is only a helper to get better
-// intellisense and typings. You can use Router.create() too.
 export const router = createPlaywrightRouter();
 
 // This replaces the request.label === DETAIL branch of the if clause.
-router.addHandler('DETAIL', async ({ request, page, log }) => {
-  log.debug(`Extracting data: ${request.url}`);
-  const urlPart = request.url.split('/').slice(-1); // ['sennheiser-mke-440-professional-stereo-shotgun-microphone-mke-440']
-  const manufacturer = urlPart[0].split('-')[0]; // 'sennheiser'
+// router.addHandler('DETAIL', async ({ request, page, log }) => {
+//   log.debug(`Extracting data: ${request.url}`);
+//   const urlPart = request.url.split('/').slice(-1); // ['sennheiser-mke-440-professional-stereo-shotgun-microphone-mke-440']
+//   const manufacturer = urlPart[0].split('-')[0]; // 'sennheiser'
 
-  const title = await page.locator('.product-meta h1').textContent();
-  const sku = await page.locator('span.product-meta__sku-number').textContent();
+//   const title = await page.locator('.product-meta h1').textContent();
+//   const sku = await page.locator('span.product-meta__sku-number').textContent();
 
-  const priceElement = page
-    .locator('span.price')
-    .filter({
-      hasText: '$',
-    })
-    .first();
+//   const priceElement = page
+//     .locator('span.price')
+//     .filter({
+//       hasText: '$',
+//     })
+//     .first();
 
-  const currentPriceString = await priceElement.textContent();
-  const rawPrice = currentPriceString.split('$')[1];
-  const price = Number(rawPrice.replaceAll(',', ''));
+//   const currentPriceString = await priceElement.textContent();
+//   const rawPrice = currentPriceString.split('$')[1];
+//   const price = Number(rawPrice.replaceAll(',', ''));
 
-  const inStockElement = page
-    .locator('span.product-form__inventory')
-    .filter({
-      hasText: 'In stock',
-    })
-    .first();
+//   const inStockElement = page
+//     .locator('span.product-form__inventory')
+//     .filter({
+//       hasText: 'In stock',
+//     })
+//     .first();
 
-  const inStock = (await inStockElement.count()) > 0;
+//   const inStock = (await inStockElement.count()) > 0;
 
-  const results = {
-    url: request.url,
-    manufacturer,
-    title,
-    sku,
-    currentPrice: price,
-    availableInStock: inStock,
-  };
+//   const results = {
+//     url: request.url,
+//     manufacturer,
+//     title,
+//     sku,
+//     currentPrice: price,
+//     availableInStock: inStock,
+//   };
 
-  log.debug(`Saving data: ${request.url}`);
-  await Dataset.pushData(results);
-});
+//   log.debug(`Saving data: ${request.url}`);
+//   await Dataset.pushData(results);
+// });
 
-router.addHandler('CATEGORY', async ({ page, enqueueLinks, request, log }) => {
-  log.debug(`Enqueueing pagination for: ${request.url}`);
-  // We are now on a category page. We can use this to paginate through and enqueue all products,
-  // as well as any subsequent pages we find
+// router.addHandler('CATEGORY', async ({ page, enqueueLinks, request, log }) => {
+//   log.debug(`Enqueueing pagination for: ${request.url}`);
+//   // We are now on a category page. We can use this to paginate through and enqueue all products,
+//   // as well as any subsequent pages we find
 
-  await page.waitForSelector('.product-item > a');
-  await enqueueLinks({
-    selector: '.product-item > a',
-    label: 'DETAIL', // <= note the different label
-  });
+//   await page.waitForSelector('.product-item > a');
+//   await enqueueLinks({
+//     selector: '.product-item > a',
+//     label: 'DETAIL', // <= note the different label
+//   });
 
-  // Now we need to find the "Next" button and enqueue the next page of results (if it exists)
-  const nextButton = await page.$('a.pagination__next');
-  if (nextButton) {
-    await enqueueLinks({
-      selector: 'a.pagination__next',
-      label: 'CATEGORY', // <= note the same label
-    });
-  }
-});
+//   // Now we need to find the "Next" button and enqueue the next page of results (if it exists)
+//   const nextButton = await page.$('a.pagination__next');
+//   if (nextButton) {
+//     await enqueueLinks({
+//       selector: 'a.pagination__next',
+//       label: 'CATEGORY', // <= note the same label
+//     });
+//   }
+// });
 
 // This is a fallback route which will handle the start URL
-// as well as the LIST labeled URLs.
 router.addDefaultHandler(async ({ request, page, enqueueLinks, log }) => {
-  log.debug(`Enqueueing categories from page: ${request.url}`);
-  // This means we're on the start page, with no label.
-  // On this page, we just want to enqueue all the category pages.
+  const searchInput = 'input[placeholder="Search store"]';
 
-  await page.waitForSelector('.collection-block-item');
-  await enqueueLinks({
-    selector: '.collection-block-item',
-    label: 'CATEGORY',
-  });
+  const searchTerm = 'Disciples II'; // exists and has two results
+  // const searchTerm = 'Papers, please'; // not found, so the shape of the dom is different,
+
+  const searchResultsContainer = 'div[data-tippy-root] ul';
+
+  await page.fill(searchInput, searchTerm);
+  await page.waitForSelector(searchResultsContainer);
+
+  // TODO: get the full list of items
+  const listOfItems = await page
+    .locator(searchResultsContainer)
+    .getByRole('listitem')
+    // works when the game is found, but need a different locator for the case that the search term has no results
+    .allTextContents();
+  console.log({ listOfItems });
+
+  enqueueLinks();
 });
+
+// for testing selectors
+const testSelector = async (selector: string, page: Page) => {
+  const targetedSelector = page.locator(selector);
+  await targetedSelector.waitFor();
+  if (targetedSelector) {
+    console.log('Targeted selector found: ', targetedSelector);
+  } else {
+    console.log('Targeted selector was NOT found');
+  }
+};
