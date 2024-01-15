d38119f5c7e7779ec36548eba43dbfb0af75ca9d
Rudi Kraeher <rudikraeher@gmail.com>
2024-01-15 01:05:13 +0100

    Add initial UI

--- src/server.ts
+++ src/server.ts
@@ -8,18 +8,29 @@ const port = 8000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const requestListener = (_req: IncomingMessage, res: ServerResponse) => {
-  fs.readFile(__dirname + '/index.html')
-    .then((contents) => {
-      res.setHeader('Content-Type', 'text/html');
-      res.writeHead(200);
-      res.end(contents);
-    })
-    .catch((err) => {
-      res.writeHead(500);
-      res.end(err);
-      return;
-    });
+const requestListener = async (req: IncomingMessage, res: ServerResponse) => {
+  let filePath = '';
+
+  if (req.url === '/' || req.url === '/index.html') {
+    filePath = path.join(__dirname, 'index.html');
+    res.setHeader('Content-Type', 'text/html');
+  } else if (req.url === '/styles.css') {
+    filePath = path.join(__dirname, 'styles.css');
+    res.setHeader('Content-Type', 'text/css');
+  } else {
+    res.writeHead(404);
+    res.end('Not Found');
+    return;
+  }
+
+  try {
+    const contents = await fs.readFile(filePath);
+    res.writeHead(200);
+    res.end(contents);
+  } catch (err: any) {
+    res.writeHead(500);
+    res.end(err.message);
+  }
 };
 
 const server = http.createServer(requestListener);
